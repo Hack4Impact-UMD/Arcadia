26f7fd8ca46c1f8d6d07bb369cd578c7c4ea1873
Miranda Song <66978315+mirandasong24@users.noreply.github.com>
2021-05-03 20:28:21 -0400

    app.py updates

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import PySimpleGUI as sg
+from pdf_creation.py import generate_PDF 
 
 # sg.theme('DarkAmber')   # Add a touch of color
 # # All the stuff inside your window.
@@ -27,12 +28,14 @@ pl_layout = [[sg.Text('Product List')],
             [sg.Text('Benefits CSV')],
             [sg.Input(), sg.FileBrowse()],
             [sg.Text('Download Location')],
-            [sg.Input(), sg.FileBrowse()], 
+            [sg.Input(), sg.FolderBrowse()], 
             [sg.OK(), sg.Cancel()]] 
 
 window = sg.Window('Get filename example', pl_layout)
-#window1 = sg.Window('Get filename example', pl_layout)
+# window1 = sg.Window('Get filename example', pl_layout)
 
 event, values = window.read()
+if event == sg.OK():
+    generate_PDF(values[0], values[1], values[2], values[3])
 window.close()
-print(values)
+# print(values)
